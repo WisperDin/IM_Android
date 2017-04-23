7398360292807ebc27ae51e4fbe5e6ef74a514ef
lzy <734800224@qq.com>
2017-04-23 22:08:16 +0800

    update login activity

--- app/src/main/java/com/cst/im/UI/LoginActivity.java
+++ app/src/main/java/com/cst/im/UI/LoginActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cst.im.R;
-import com.cst.im.UI.main.chat.ChatActivity;
+import com.cst.im.UI.main.MainActivity;
 import com.cst.im.presenter.ILoginPresenter;
 import com.cst.im.presenter.LoginPresenterCompl;
 import com.cst.im.view.ILoginView;
@@ -71,7 +71,7 @@ public class LoginActivity extends AppCompatActivity implements ILoginView {
     public void onLoginResult(Boolean result, int code){
         if (result){
             //页面跳转
-            Intent it = new Intent(LoginActivity.this, ChatActivity.class);
+            Intent it = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(it);
             Toast.makeText(this,"Login Success", Toast.LENGTH_SHORT).show();
         }
