63128c6b21a517092b39285d17655d39c3283eb7
lzy <734800224@qq.com>
2017-04-23 20:13:00 +0800

    update login activity

--- app/src/main/java/com/cst/im/UI/LoginActivity.java
+++ app/src/main/java/com/cst/im/UI/LoginActivity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cst.im.R;
 import com.cst.im.UI.main.chat.ChatActivity;
 import com.cst.im.presenter.ILoginPresenter;
 import com.cst.im.presenter.LoginPresenterCompl;
@@ -18,6 +24,18 @@ public class LoginActivity extends AppCompatActivity implements ILoginView {
     private EditText editPwd;
     private Button btnLogin;
 
+
+    private ImageView topPicture ;
+    private EditText userName;
+    private EditText passWord;
+    private ImageView showPassword;
+    private Button loginButton;
+    private Button loginByMessage;
+    private ImageView loginByQQ;
+    private ImageView loginByWechat;
+    private ImageView loginByWeibo;
+    private TextView forgetPassword;
+    private LinearLayout activityMain ;
     //登录业务逻辑
     ILoginPresenter loginPresenter;
     @Override
@@ -29,17 +47,26 @@ public class LoginActivity extends AppCompatActivity implements ILoginView {
         loginPresenter = new LoginPresenterCompl(this);
 
         //find Control
-        editUser=(EditText) findViewById(com.cst.im.R.id.login_account);
-        editPwd=(EditText) findViewById(com.cst.im.R.id.login_password);
-        btnLogin=(Button) findViewById(com.cst.im.R.id.login_button);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        topPicture = (ImageView) findViewById(R.id.top_picture);
+        userName = (EditText) findViewById(R.id.username);
+        passWord = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.login_button);
+        loginByMessage = (Button) findViewById(R.id.login_message);
+        loginByQQ = (ImageView) findViewById(R.id.qq_login);
+        loginByWechat = (ImageView) findViewById(R.id.wechat_login);
+        loginByWeibo = (ImageView) findViewById(R.id.weibo_login);
+        activityMain = (LinearLayout) findViewById(R.id.activity_main);
+        //set btn listener
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loginPresenter.doLogin(editUser.getText().toString(),editPwd.getText().toString());
+                loginPresenter.doLogin(userName.getText().toString(),passWord.getText().toString());
             }
         });
+
+        HandleLoginAnimator();
     }
-    //处理登录事件
+    //处理登录事件的UI提示
     @Override
     public void onLoginResult(Boolean result, int code){
         if (result){
@@ -50,5 +77,34 @@ public class LoginActivity extends AppCompatActivity implements ILoginView {
         }
         else
             Toast.makeText(this,"Login Fail, code = " + code,Toast.LENGTH_SHORT).show();
+    }
+    //处理登录动画
+    private void HandleLoginAnimator(){
+        //显示动画
+        final TranslateAnimation mShowAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                -1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        mShowAction.setDuration(500);
+
+        //隐藏动画
+        final TranslateAnimation mHiddenAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                -1.0f);
+        mHiddenAction.setDuration(1000);
+        passWord.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus){
+//                    topPicture.startAnimation(mHiddenAction);
+                    topPicture.setVisibility(View.GONE);
+                }
+                else{
+                    topPicture.startAnimation(mShowAction);
+                    topPicture.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 }
