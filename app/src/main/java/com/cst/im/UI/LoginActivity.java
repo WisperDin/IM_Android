9f5296649f13bfaf8d6bf44f4ba7ff8b5ed8092c
lzy <734800224@qq.com>
2017-04-24 21:21:18 +0800

    增加espresso UItest框架

--- app/src/main/java/com/cst/im/UI/LoginActivity.java
+++ app/src/main/java/com/cst/im/UI/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
