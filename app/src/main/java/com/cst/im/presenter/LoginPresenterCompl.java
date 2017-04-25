3cde12e01026ebb1dd7db290121d0d4f9fe9085f
Acring <392398434@qq.com>
2017-04-25 17:06:51 +0800

    补全类

--- app/src/main/java/com/cst/im/presenter/LoginPresenterCompl.java
+++ app/src/main/java/com/cst/im/presenter/LoginPresenterCompl.java
@@ -3,7 +3,9 @@ package com.cst.im.presenter;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.cst.im.model.ILoginUser;
 import com.cst.im.model.IUser;
+import com.cst.im.model.LoginUserModel;
 import com.cst.im.model.UserModel;
 import com.cst.im.view.ILoginView;
 /**
@@ -11,7 +13,7 @@ import com.cst.im.view.ILoginView;
 
 public class LoginPresenterCompl implements ILoginPresenter {
     ILoginView iLoginView;
-    IUser user;
+    ILoginUser user;
     Handler    handler;
 
     public LoginPresenterCompl(ILoginView iLoginView) {
@@ -19,7 +21,10 @@ public class LoginPresenterCompl implements ILoginPresenter {
         initUser();
         handler = new Handler(Looper.getMainLooper());
     }
+    @Override
+    public void saveLoginInf() {
 
+    }
     @Override
     public int doLogin(String name, String passwd) {
         Boolean isLoginSuccess = true;
@@ -35,6 +40,31 @@ public class LoginPresenterCompl implements ILoginPresenter {
         return code;
     }
     private void initUser(){
-        user = new UserModel("mvp","mvp");
+        user = new LoginUserModel("mvp","mvp");
+    }
+
+    @Override
+    public boolean doOtherLogin() {
+        return false;
+    }
+
+    @Override
+    public boolean doRegister() {
+        return false;
+    }
+
+    @Override
+    public void doForgetPassword() {
+
+    }
+
+    @Override
+    public short judgeUsername(String username) {
+        return user.checkTypeOfUsername(username);
+    }
+
+    @Override
+    public boolean judgePassword(String password) {
+        return user.checkPasswordValidity(password);
     }
 }
--- app/src/main/java/com/cst/im/view/ILoginView.java
+++ app/src/main/java/com/cst/im/view/ILoginView.java
@@ -1,5 +1,7 @@
 package com.cst.im.view;
 
+import android.widget.EditText;
+
 /**
  * Created by ASUS on 2017/4/23.
  */
