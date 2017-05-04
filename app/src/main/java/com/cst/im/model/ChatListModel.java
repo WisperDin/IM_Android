32016b2c350aeb0f2d35762c7e64a232ca1ec686
Carl <1622452328@qq.com>
2017-05-05 00:24:25 +0800

    消息列表item可操作，部分已读未读完成

--- app/src/main/java/com/cst/im/model/ChatListModel.java
+++ app/src/main/java/com/cst/im/model/ChatListModel.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
  */
 
 public class ChatListModel implements IChatList {
-    //储存消息对象，key:Sender To Accepter
+    //储存消息对象，key:AccepterID
     HashMap<String,ChatItem> MsgList=null;
     public ChatListModel(){
         MsgList=new HashMap<String,ChatItem>();
@@ -18,24 +18,28 @@ public class ChatListModel implements IChatList {
     //新增一个消息对象
     @Override
     public void addChatItem(String key, ChatItem chatItem) {
-
+        MsgList.put(key, chatItem);
     }
 
     //删除一个消息对象
     @Override
     public void deleteChatItem(String key) {
-
+        MsgList.remove(key);
     }
 
     //获取指定的消息对象
     @Override
     public ChatItem getChatItem(String key) {
-        return null;
+        return MsgList.get(key);
     }
 
     //判断某个消息对象是否存在
     @Override
     public boolean checkChatItem(String key) {
-        return false;
+        boolean ok=false;
+        if(MsgList.containsKey(key)){
+            ok=true;
+        }
+        return ok;
     }
 }
