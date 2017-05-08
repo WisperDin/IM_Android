94b8159b754c64a5bff3310c5e9137d2fa397ff4
polluxlee <polluxlee@163.com>
2017-05-08 20:31:37 +0800

    修改登录注册界面

--- app/src/main/java/com/cst/im/presenter/Status.java
+++ app/src/main/java/com/cst/im/presenter/Status.java
@@ -12,7 +12,9 @@ public final class Status {
         public static final int USERNAME_EMAIL = 2; // 用户名为邮箱
         public static final int USERNAME_ACCOUNT = 3; // 用户名为普通账号
         public static final int LOGINSUCCESS = 200; // 登录成功
-        public static final int LOGINFAILED = 201; // 用户名为普通账号
+        public static final int LOGINFAILED = 201; // 登录失败
+        public static final int LOGINNOTEXIST = 202; // 不存在用户
+        public static final int LOGINWRONGPWD = 203; // 用户存在，密码错误
     }
 
     public static final class Register{
