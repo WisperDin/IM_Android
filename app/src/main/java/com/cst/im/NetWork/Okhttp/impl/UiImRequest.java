3b16b315d0d4d0750fb7e6e91573711eca08e1c4
cjwddz <cjwddz@qq.com>
2017-05-08 23:02:01 +0800

    网络请求对象

--- app/src/main/java/com/cst/im/NetWork/Okhttp/impl/UiImRequest.java
+++ app/src/main/java/com/cst/im/NetWork/Okhttp/impl/UiImRequest.java
@@ -1,73 +1,120 @@
 package com.cst.im.NetWork.Okhttp.impl;
 
-import android.os.Handler;
+import com.cst.im.FileAccess.FileSweet;
+import com.cst.im.NetWork.Okhttp.listener.ProgressListener;
+import com.cst.im.NetWork.Okhttp.progress.ProgressRequestBody;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 /**
  * Created by admin on 2017/5/8.
  */
 
-public abstract class UiImRequest implements ImRequest {
-    private boolean isFirst = false;
-
-    //处理UI层的Handler子类
-    private static class UIHandler extends MProgressHandler {
-        public UIHandler(UiImRequest uiImRequest) {
-            super(uiImRequest);
-        }
-
-        @Override
-        public void start(UiImRequest uiImRequest, long currentBytes, long contentLength, boolean done) {
-            if (uiImRequest!=null) {
-                uiImRequest.onUIStart(currentBytes, contentLength, done);
+public class UiImRequest implements ImRequest {
+    static String url="";
+    OkHttpClient client=new OkHttpClient();
+    @Override
+    public void preRequest(FileSweet fileSweet, final ResultCallBack callback) {
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("name",fileSweet.getFileName())
+                .addFormDataPart("type", ""+fileSweet.getFileType())
+                .addFormDataPart("parem", fileSweet.getFileParam())
+                .build();
+        Request request = new Request.Builder()
+                .url(this.url)
+                .post(requestBody)
+                .build();
+        Call call= client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                callback.fail(-1,"网络连接失败，请检查网络！");
             }
-        }
-
-        @Override
-        public void progress(UiImRequest uiImRequest, long currentBytes, long contentLength, boolean done) {
-            if (uiImRequest!=null){
-                uiImRequest.onUIProgress(currentBytes, contentLength, done);
+            @Override
+            public void onResponse(Response response) throws IOException {
+                callback.success(response.code(),response.message());
             }
+        });
+        try {
+            call.execute();
+        } catch (IOException e) {
+            callback.fail(-2,"IO错误！--"+e.toString());
         }
-
-        @Override
-        public void finish(UiImRequest uiImRequest, long currentBytes, long contentLength, boolean done) {
-            if (uiImRequest!=null){
-                uiImRequest.onUIFinish(currentBytes, contentLength,done);
-            }
-        }
-
-    }
-    //主线程Handler
-    private final Handler mHandler = new UIHandler(this);
-
-
-    /**
-     * UI层回调抽象方法
-     *
-     * @param currentBytes    当前的字节长度
-     * @param contentLength 总字节长度
-     * @param done          是否写入完成
-     */
-    public abstract void onUIProgress(long currentBytes, long contentLength, boolean done);
-
-    /**
-     * UI层开始请求回调方法
-     * @param currentBytes 当前的字节长度
-     * @param contentLength 总字节长度
-     * @param done 是否写入完成
-     */
-    public void onUIStart(long currentBytes, long contentLength, boolean done) {
-
     }
 
-    /**
-     * UI层结束请求回调方法
-     * @param currentBytes 当前的字节长度
-     * @param contentLength 总字节长度
-     * @param done 是否写入完成
-     */
-    public void onUIFinish(long currentBytes, long contentLength, boolean done) {
+    @Override
+    public void upLoadFile(FileSweet fileSweet, final ResultCallBack resultCallback) {
+        RequestBody fileBody = RequestBody.create(MediaType.parse("file"), fileSweet.getFile());
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", fileSweet.getFileName(), fileBody)
+                .addFormDataPart("name",fileSweet.getFileName())
+                .addFormDataPart("type", ""+fileSweet.getFileType())
+                .addFormDataPart("parem", fileSweet.getFileParam())
+                .build();
+        Request request = new Request.Builder()
+                .url(this.url)
+                .post(requestBody)
+                .build();
+        Call call= client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                resultCallback.fail(-1,"网络连接失败，请检查网络！");
+            }
+            @Override
+            public void onResponse(Response response) throws IOException {
+                resultCallback.success(response.code(),response.message());
+            }
+        });
+        try {
+            call.execute();
+        } catch (IOException e) {
+            resultCallback.fail(-2,"IO错误！--"+e.toString());
+        }
+    }
 
+    @Override
+    public void upLoadFile(FileSweet fileSweet, final ProccessCallBack processCallback) {
+        RequestBody fileBody = RequestBody.create(MediaType.parse("file"), fileSweet.getFile());
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", fileSweet.getFileName(), fileBody)
+                .addFormDataPart("name",fileSweet.getFileName())
+                .addFormDataPart("type", ""+fileSweet.getFileType())
+                .addFormDataPart("parem", fileSweet.getFileParam())
+                .build();
+        ProgressRequestBody progressRequestBody = new ProgressRequestBody(requestBody, new ProgressListener() {
+            @Override
+            public void onProgress(long currentBytes, long contentLength, boolean done) {
+                if(done){
+                    processCallback.success();
+                    return;
+                }
+                processCallback.onProgress((double) currentBytes/contentLength);
+            }
+        });
+        Request request = new Request.Builder()
+                .url(this.url)
+                .post(progressRequestBody)
+                .build();
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            processCallback.fail();
+        }
     }
 }
