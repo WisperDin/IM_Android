45f9cfc0b8cc4358e7edbbea89bb6c2c691df500
sun <1003008727@qq.com>
2017-05-13 13:24:36 +0800

    修改搜索不合法输入的bug

--- app/src/main/java/com/cst/im/UI/main/friend/FriendViewFragment.java
+++ app/src/main/java/com/cst/im/UI/main/friend/FriendViewFragment.java
@@ -3,6 +3,7 @@ package com.cst.im.UI.main.friend;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -18,47 +19,52 @@ import com.cst.im.model.IFriendModel;
 
 import java.util.ArrayList;
 
+import static com.cst.im.model.UserModel.localUser;
 
 
 public class FriendViewFragment extends Fragment implements
         AdapterView.OnItemClickListener,
         AdapterView.OnItemLongClickListener {
-    MyCustomAdapter adapter=null;
-    private  ArrayList<Integer> letter = new ArrayList<Integer>();//储存标题上的大写字母的位置
-     ArrayList<String> NameSequencebylistview= new ArrayList<String>();//记录item上的名字
+    MyCustomAdapter adapter = null;
+    private ArrayList<Integer> letter = new ArrayList<Integer>();//储存标题上的大写字母的位置
+    ArrayList<String> NameSequencebylistview = new ArrayList<String>();//记录item上的名字
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friends, container, false);
-        ListView Friendlistview = (ListView)  view.findViewById(R.id.lv_friend);
-        ImageView img_search=(ImageView)view.findViewById(R.id.img_search);
-        InitView(img_search,Friendlistview);
+        ListView Friendlistview = (ListView) view.findViewById(R.id.lv_friend);
+        ImageView img_search = (ImageView) view.findViewById(R.id.img_search);
+        InitView(img_search, Friendlistview);
         Friendlistview.setOnItemClickListener(this);    //设置单击监听，接口实现
         Friendlistview.setOnItemLongClickListener(this);  //设置长按监听，接口实现
         img_search.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SearchFriend.class);
-                Bundle bundle=new Bundle();
-                bundle.putInt("srcId",IFriendModel.iFriendModel.getId());
-                intent.putExtras(bundle);
-                getActivity().startActivity(intent);
+                int userid = localUser.getId();
+                if (userid != 0) {
+                    Intent intent = new Intent(getActivity(), SearchFriend.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("srcId", userid);
+                    intent.putExtras(bundle);
+                    getActivity().startActivity(intent);
+                }
             }
         });
         return view;
     }
 
 
-    public void InitView(ImageView img,ListView lv){
+    public void InitView(ImageView img, ListView lv) {
         adapter = new MyCustomAdapter(this.getActivity());
-        AddFriendName addfriend=new AddFriendName();
-        if(IFriendModel.iFriendModel==null){
-            Log.w("FriendViewFragment","InitView iFriendModel null");
+        AddFriendName addfriend = new AddFriendName();
+        if (IFriendModel.iFriendModel == null) {
+            Log.w("FriendViewFragment", "InitView iFriendModel null");
             return;
         }
-        int size=IFriendModel.iFriendModel.getfriendlist().size();
-        for(int i = 0; i< size; i++){
+        int size = IFriendModel.iFriendModel.getfriendlist().size();
+        for (int i = 0; i < size; i++) {
             addfriend.SortAndAdd(IFriendModel.iFriendModel.getfriendlist().get(i));
         }
         for (int i = 0; i < addfriend.getTittle().length; i++) {
@@ -66,13 +72,13 @@ public class FriendViewFragment extends Fragment implements
             if (addfriend.getFriendname().get(addfriend.getTittle()[i]).size() != 0) {
                 adapter.addSeparatorItem(addfriend.getTittle()[i], R.drawable.friend_icon);
                 NameSequencebylistview.add(addfriend.getTittle()[i]);
-                for (int k = 0; k <addfriend.getFriendname().get(addfriend.getTittle()[i]).size(); k++) {
+                for (int k = 0; k < addfriend.getFriendname().get(addfriend.getTittle()[i]).size(); k++) {
                     adapter.addItem(addfriend.getFriendname().get(addfriend.getTittle()[i]).get(k), R.drawable.friend_icon);
                     NameSequencebylistview.add(addfriend.getFriendname().get(addfriend.getTittle()[i]).get(k));
                 }
             }
         }
-        letter=adapter.letterList;
+        letter = adapter.letterList;
         lv.setAdapter(adapter);
     }
 
@@ -85,15 +91,15 @@ public class FriendViewFragment extends Fragment implements
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position,
                             long id) {
-        for (int i=0;i<letter.size();i++){
-            if(position==letter.get(i)){
+        for (int i = 0; i < letter.size(); i++) {
+            if (position == letter.get(i)) {
                 return;
             }
         }
         Intent intent = new Intent(getActivity(), friendinformationActivity.class);
-        Bundle bundle=new Bundle();
-        bundle.putString("dstName",NameSequencebylistview.get(position));
-        bundle.putInt("dstId",IFriendModel.iFriendModel.getFriendNameAndID().get(NameSequencebylistview.get(position)));
+        Bundle bundle = new Bundle();
+        bundle.putString("dstName", NameSequencebylistview.get(position));
+        bundle.putInt("dstId", IFriendModel.iFriendModel.getFriendNameAndID().get(NameSequencebylistview.get(position)));
         intent.putExtras(bundle);
         getActivity().startActivity(intent);
 
@@ -102,13 +108,13 @@ public class FriendViewFragment extends Fragment implements
     @Override
     public boolean onItemLongClick(AdapterView<?> parent, View view,
                                    int position, long id) {
-        for (int i=0;i<letter.size();i++){
-            if(position==letter.get(i)){
+        for (int i = 0; i < letter.size(); i++) {
+            if (position == letter.get(i)) {
                 return false;
             }
         }
         final Customdialog.Builder builder = new Customdialog.Builder(getActivity());
-        final Dialog dialog=builder.create();
+        final Dialog dialog = builder.create();
         dialog.show();
         builder.gettextview().setOnClickListener(new View.OnClickListener() {
             @Override
