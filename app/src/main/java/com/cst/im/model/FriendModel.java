f3726a7588912411d95edebe44388e42d1a236f5
sun <1003008727@qq.com>
2017-05-14 19:57:53 +0800

    小bug

--- app/src/main/java/com/cst/im/model/FriendModel.java
+++ app/src/main/java/com/cst/im/model/FriendModel.java
@@ -35,7 +35,7 @@ public class FriendModel implements IFriend{
     //好友ID，名字的map
     private HashMap<String ,Integer> friendNameAndID = new HashMap<String , Integer>();
 
-    public static FriendModel friendModel;
+    public static FriendModel friendModel=new FriendModel();
     public static void InitFriendModel(ArrayList<String> friendlist,HashMap<String ,Integer> NameAndID){
         friendModel =new FriendModel(friendlist,NameAndID);
     }
