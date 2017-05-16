e065438f77e39acfe90abbf880151f293fa8d1c1
lzy <734800224@qq.com>
2017-05-16 14:56:35 +0800

    补充一个单元测试

---
+++ app/src/test/java/com/cst/im/presenter/RecvChatMsgTest.java
@@ -0,0 +1,58 @@
+package com.cst.im.presenter;
+
+import com.cst.im.model.IBaseMsg;
+import com.cst.im.model.UserModel;
+import com.cst.im.view.IChatView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by ASUS on 2017/5/16.
+ */
+
+public class RecvChatMsgTest {
+
+    //登录业务逻辑
+    private static ChatPresenter chatPresenter;
+    @Mock
+    private IChatView iChatViewView;
+
+    @Before
+    public void setup()throws Exception{
+        //init login presenter
+        List<IBaseMsg> msg = new ArrayList<IBaseMsg>();
+        UserModel a = new UserModel();
+        a.setId(1);
+        UserModel b = new UserModel();
+        b.setId(3);
+        UserModel c = new UserModel();
+        c.setId(2);
+        UserModel[] dst = new UserModel[]{a,b,c};
+
+        chatPresenter=new ChatPresenter(iChatViewView,msg,dst);
+    }
+    //    @Test
+//    public void login_Failed() throws Exception {
+//        int errCode = loginPresenter.doLogin("123","456");
+//        assertEquals(errCode, -1);
+//    }
+//
+//
+   @Test
+    public void CheckSrcId() throws Exception {
+        assertEquals(true,chatPresenter.CheckSrcID(1));
+    }
+    @Test
+    public void CheckDstId() throws Exception {
+        UserModel.localUser = new UserModel();
+        UserModel.localUser.setId(1);
+        assertEquals(true,chatPresenter.CheckDstID(new int[]{1,2,3}));
+    }
+}
